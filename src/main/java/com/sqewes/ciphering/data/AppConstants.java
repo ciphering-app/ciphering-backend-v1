1defcb04b31283be2430be3df65039e8d0a26531
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2025-08-05 23:30:26 +0530

    Spring Security Setup

---
+++ src/main/java/com/sqewes/ciphering/data/AppConstants.java
@@ -0,0 +1,11 @@
+package com.sqewes.ciphering.data;
+
+public class AppConstants {
+
+    private AppConstants(){}
+
+    public static final String USER_REGISTERED = "User registered successfully!";
+    public static final String MESSAGE = "message";
+    public static final String INVALID_TOKEN = "Invalid Token";
+
+}
